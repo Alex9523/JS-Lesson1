11218cfe5b2c0b99ca05c7c00d9caa62a0b3206d
Aleksandr <syaskiy9523@gmail.com>
2018-02-28 17:29:41 +0000

    initial2

--- js/index.js
+++ js/index.js
@@ -1,34 +1,33 @@
 function discr(a, b, c) {
-    document.writeln("Your equation : " + a + "x*x + (" + b + "x) + (" + c + ") = 0");
-
-    let first_x = null;
-    let second_x = null;
-    let discriminant = Math.pow(b, 2) - 4 * a * c;
-    console.log(discriminant);
-
-    if (a == 0) {
-        first_x = -c / b;
-        return [first_x, second_x];
-    }
-    else if (discriminant > 0) {
-        first_x = (-1 * b) + (Math.sqrt(discriminant));
-        second_x = (-1 * b) - (Math.sqrt(discriminant));
-        return [first_x, second_x];
-    }
-    else if (discriminant == 0) {
-        first_x = -b / (2 * a);
-        return [first_x, second_x];
-    }
-    else {
-        document.writeln("discriminant < 0");
-        return null;
-    }
+    const discriminant = Math.pow(b, 2) - 4 * a * c;
+    return discriminant;
 }
 
-let digit_a = prompt('Enter digit a', 0);
-let digit_b = prompt('Enter digit b', 0);
-let digit_c = prompt('Enter digit c', 0);
+function checkDiscr(discriminant ,a ,b, c)
+{
+    let firstX;
+    let secondX;
+    if (discriminant > 0) 
+    {
+        firstX = ((-1 * b) + (Math.sqrt(discriminant)))/ (2*a);
+        secondX = ((-1 * b) - (Math.sqrt(discriminant)))/ (2*a);
+        return [firstX, secondX];
+    }
+    else if (discriminant === 0) 
+    {
+        firstX = -b / (2 * a);
+        return [firstX];//вертає наші корені рівнняня
+    }
+        document.write("discriminant < 0");
+        return null;
+   
+}
 
-let [x, y] = discr(digit_a, digit_b, digit_c);
-document.writeln("First x = " + x);
+//вводимо числа з клавіатури
+let digitA = prompt('Enter digit a', 0);
+let digitB = prompt('Enter digit b', 0);
+let digitC = prompt('Enter digit c', 0);
+let d = discr(digitA, digitB, digitC);
+let [x  ,y] = checkDiscr(d,digitA, digitB, digitC);
+document.write("First x = " + x);
 document.write("Second x = " + y);
